d6b7989e236cdf97f8376f5b3e4b9797ceb9ac1f
Omar Bakier <omarbakeer@gmail.com>
2018-12-27 21:19:26 +0200

    init connection with firebase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Router from "./Routes";
+import { initializeApp } from "firebase";
 import * as serviceWorker from "./serviceWorker";
+
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID
+} = process.env;
+
+initializeApp({
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID
+});
 
 ReactDOM.render(<Router />, document.getElementById("root"));
 
