757f7071117823949956503e70c42b9326a6e307
Omar Bakier <omarbakeer@gmail.com>
2018-12-27 12:36:31 +0200

    ui-dev list screen

--- src/screens/App/index.js
+++ src/screens/App/index.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { withRouter } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import Typography from "@material-ui/core/Typography";
 
 class App extends Component {
   render() {
     return (
       <Grid
         container
+        direction="column"
         justify="center"
         alignItems="center"
         style={{ height: "100vh" }}
       >
+        <Button
+          onClick={() => this.props.history.push("/list")}
+          style={{ position: "absolute", top: "70px", right: "20%" }}
+        >
+          Go to the list <ArrowForwardIcon />{" "}
+        </Button>
         <Card
           style={{
             display: "flex",
@@ -46,6 +57,9 @@ class App extends Component {
             <StyledContent>5</StyledContent>
           </Grid>
         </Card>
+        <Typography style={{ marginTop: "30px" }}>
+          Made by Omar Bakier
+        </Typography>
       </Grid>
     );
   }
@@ -65,4 +79,4 @@ const Pipe = styled.div`
   }
 `;
 
-export default App;
+export default withRouter(App);
--- src/screens/List/index.js
+++ src/screens/List/index.js
@@ -1,7 +1,62 @@
 import React, { Component } from "react";
-
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import List from "@material-ui/core/List";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Button from "@material-ui/core/Button";
+import ArrowBack from "@material-ui/icons/ArrowBack";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import Typography from "@material-ui/core/Typography";
 export default class Routes extends Component {
   render() {
-    return <h1>List</h1>;
+    return (
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        style={{ height: "100vh" }}
+      >
+        <Button
+          onClick={() => this.props.history.push("/")}
+          style={{ position: "absolute", top: "70px", left: "10%" }}
+        >
+          <ArrowBack />
+          Go to the App
+        </Button>
+        <Card style={{ width: "50vw" }}>
+          <List
+            subheader={
+              <ListSubheader
+                color="primary"
+                component="h1"
+                style={{ fontSize: "28px" }}
+              >
+                Timestamp List
+              </ListSubheader>
+            }
+          >
+            <ListItem divider button selected>
+              <ListItemIcon>
+                <AccessTimeIcon />
+              </ListItemIcon>
+              <ListItemText>Hello world</ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <AccessTimeIcon />
+              </ListItemIcon>
+              <ListItemText>Hello world</ListItemText>
+            </ListItem>
+          </List>
+        </Card>
+        <Typography style={{ marginTop: "30px" }}>
+          Made by Omar Bakier
+        </Typography>
+      </Grid>
+    );
   }
 }
