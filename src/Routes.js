3a12a13d8fdc86f18c154f2698e3cd4397a960fb
Omar Bakier <omarbakeer@gmail.com>
2018-12-28 02:16:35 +0200

    added basic functionalities to the list screen

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./screens/App";
-import List from "./screens/List";
+import TimestampList from "./screens/List";
 import NotFound from "./screens/NotFound404";
 
 export default class Routes extends Component {
@@ -10,7 +10,7 @@ export default class Routes extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={App} />
-          <Route exact path="/list" component={List} />
+          <Route exact path="/list" component={TimestampList} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
--- src/screens/List/index.js
+++ src/screens/List/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import * as firebase from "firebase";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import List from "@material-ui/core/List";
@@ -10,25 +13,55 @@ import Button from "@material-ui/core/Button";
 import ArrowBack from "@material-ui/icons/ArrowBack";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export default class TimestampList extends Component {
+  static propTypes = {
+    history: PropTypes.object
+  };
+
+  state = {
+    timeStamps: []
+  };
+
+  componentDidMount = async () => {
+    try {
+      const arr = [];
+      const snapshot = await firebase
+        .database()
+        .ref("timeStamps")
+        .once("value");
+      snapshot.forEach(child => {
+        arr.push(child.val());
+      });
+      this.setState(() => ({ timeStamps: [...arr] }));
+      console.warn(this.state.timeStamps);
+    } catch (e) {
+      console.warn(e);
+    }
+  };
+
   render() {
+    const { timeStamps } = this.state;
     return (
       <Grid
         container
         direction="column"
         justify="center"
         alignItems="center"
-        style={{ marginTop: "30px", marginBottom: "20px" }}
+        style={{ margin: "30px 0 20px" }}
       >
-        <Button
-          onClick={() => this.props.history.push("/")}
-          style={{ position: "relative", right: "20%", marginBottom: "20px" }}
-        >
-          <ArrowBack />
-          Go to the App
-        </Button>
-        <Card style={{ width: "50vw", height: "100%" }}>
+        <Grid container justify="flex-start" style={{ width: "40vw" }}>
+          <Button
+            variant="outlined"
+            onClick={() => this.props.history.push("/")}
+            style={{ marginBottom: "20px" }}
+          >
+            <ArrowBack />
+            Go to the App
+          </Button>
+        </Grid>
+        <StyledCard>
           <List
             subheader={
               <ListSubheader
@@ -40,24 +73,29 @@ export default class TimestampList extends Component {
               </ListSubheader>
             }
           >
-            <ListItem divider button selected>
-              <ListItemIcon>
-                <AccessTimeIcon />
-              </ListItemIcon>
-              <ListItemText>Hello world</ListItemText>
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon>
-                <AccessTimeIcon />
-              </ListItemIcon>
-              <ListItemText>Hello world</ListItemText>
-            </ListItem>
+            {timeStamps.length > 0 ? (
+              timeStamps.map((item, index) => (
+                <ListItem key={index} selected={index % 2 === 0 ? true : false}>
+                  <ListItemIcon>
+                    <AccessTimeIcon />
+                  </ListItemIcon>
+                  <ListItemText>{item}</ListItemText>
+                </ListItem>
+              ))
+            ) : timeStamps.length === 0 ? null : (
+              <CircularProgress />
+            )}
           </List>
-        </Card>
-        <Typography style={{ marginTop: "30px" }}>
-          Made by Omar Bakier
-        </Typography>
+        </StyledCard>
+        <Typography style={{ marginTop: "30px" }}>Made by O.Bakier</Typography>
       </Grid>
     );
   }
 }
+
+const StyledCard = styled(Card)`
+  && {
+    width: 40vw;
+    height: 100%;
+  }
+`;
